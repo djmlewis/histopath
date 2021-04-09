9bcae78c429231712967beee2483c86bb66673dc
djmlewis <djmlewis@mac.com>
2021-04-09 19:25:47 +0100

    ``
    Signed-off-by: djmlewis <djmlewis@mac.com>

--- myscripts/helpMenu.3.js
+++ myscripts/helpMenu.3.js
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 19/2/2021 4:1     djml.uk E&OE.                              *
+ * Copyright (c) 9/4/2021 7:25     djml.uk E&OE.                              *
  ******************************************************************************/
 function toggleActionmenuToolbar(fromdropdown) {
     if(!!fromdropdown)$(fromdropdown).dropdown('hide');
@@ -8,17 +8,11 @@ function toggleActionmenuToolbar(fromdropdown) {
 
 
 function toggleUserGuide(anchorName) {
-    if (locationType() !== runningLocalFile) {
-        ["div-mainpage","div-userGuide"].forEach(e=>document.getElementById(e).toggleAttribute("hidden"));
-        const anchor = document.querySelector("a[name='"+anchorName+"']");
-        if(!!anchor) anchor.scrollIntoView();
-        //because a screen rotation or resize when the image is obscured results in a blank image - force a redraw anyway:
-        if(document.getElementById("div-mainpage").hidden === false) adjustCardWidthHeight();
-    }
-    else openLocalHelp(anchorName);
+    window.open("userGuide.html", "Parasites Atlas User Guide");
 }
 
 function loadHelp(){
+/*
     if (locationType() !== runningLocalFile) {
         fetch("userGuide.html")
             .then(checkStatus)
@@ -30,8 +24,10 @@ function loadHelp(){
                 console.log(e);
             });
     }
+*/
 }
 
+/*
 function openLocalHelp() {
     window.open("userGuide.html", "Parasites Atlas User Guide");
-}
+}*/
