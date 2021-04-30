14ede9da5ef7905b1899e039751d84a9242bd3f6
djmlewis <djmlewis@mac.com>
2021-04-30 13:25:00 +0100

    ``
    Signed-off-by: djmlewis <djmlewis@mac.com>

--- myscripts/answers.17.js
+++ myscripts/answers.17.js
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 30/4/2021 1:23     djml.uk E&OE.                             *
+ * Copyright (c) 30/4/2021 1:25     djml.uk E&OE.                             *
  ******************************************************************************/
 
 // =================================================================== AnswersRecord
@@ -578,7 +578,7 @@ function addLegendArrayToDiv(selectedCardObj) {
 
         '<div class="row d-flex align-items-center bg-white" style="border-top: 1px solid #eaeaea">' +
         '<div class="col-3 legend-subhead">' +
-        '' +
+        'Life Cycle Diagram' +
         '</div>' +
         '<div class="col legend-text bg-white" style="padding-bottom: 16px;visibility: ' + (initiallyHideText ? 'hidden' : 'visible') + ';">' +
         '<img src="'+ imgSrc +
